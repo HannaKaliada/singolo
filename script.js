1b02588b6e42f0d4af1ae738c00cd299bea56040
HannaKaliada <kaledaania2411@gmail.com>
2020-03-24 18:01:51 +0300

    feat: add responsive task

--- script.js
+++ script.js
@@ -2,12 +2,18 @@
     const site = {
         activeLinks() {
             const MENU = document.querySelector('#nav');
+            const MOBILE_MENU = document.querySelector('#mobile-menu');
+            const BURGER_BTN = document.querySelector('#burger-menu-btn');
+            const LOGO = document.querySelector("#logo");
             MENU.addEventListener('click', (event) => {
                 if (event.target.classList.contains('link')) {
-                    MENU.querySelectorAll('#navItem').forEach(function (link) {
+                    MENU.querySelectorAll('#navItem').forEach((link) => {
                         link.classList.remove('active');
                     })
                     event.target.classList.add('active');
+                    MOBILE_MENU.classList.remove('opened');
+                    BURGER_BTN.classList.remove('rotate');
+                    LOGO.classList.remove('lefter');
                     this.scrollToSection(event.target);
                 }
             });
@@ -19,14 +25,31 @@
         changeActiveLinkDuringScroll() {
             const sections = document.querySelectorAll("section");
             const navLinks = document.querySelectorAll("#navItem");
-            const sectionMargin = 95;
+            const sectionMargin = 89;
             let currentActive = 0;
             window.addEventListener('scroll', () => {
                 const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin) - 1;
                 if (current !== currentActive) {
-                    navLinks.forEach((link)=>link.classList.remove('active'));
+                    navLinks.forEach((link) => link.classList.remove('active'));
                     currentActive = current;
                     navLinks[current].classList.add("active");
+                }
+            });
+        },
+        mobileMenu() {
+            const BURGER_BTN = document.querySelector('#burger-menu-btn');
+            const MOBILE_MENU = document.querySelector('#mobile-menu');
+            const LOGO = document.querySelector("#logo");
+            BURGER_BTN.addEventListener('click', () => {
+                if (BURGER_BTN.classList.contains('rotate')) {
+                    BURGER_BTN.classList.remove('rotate');
+                    LOGO.classList.remove('lefter');
+                    MOBILE_MENU.classList.remove('opened');
+                }
+                else {
+                    BURGER_BTN.classList.add('rotate');
+                    LOGO.classList.add('lefter');
+                    MOBILE_MENU.classList.add('opened');
                 }
             });
         },
@@ -95,6 +118,110 @@
                     nextSlide(currentSlide);
                 }
             });
+            let el = document.querySelector('#slider');
+            function swipeDetect(el) {
+                let surface = el;
+                let startX = 0;
+                let startY = 0;
+                let distX = 0;
+                let distY = 0;
+
+                let startTime = 0;
+                let elapsedTime = 0;
+
+                let threshold = 150;
+                let restraint = 100;
+                let allowedTime = 300;
+
+                // surface.addEventListener('mousedown', (e) => {
+                //     startX = e.pageX;
+                //     startY = e.pageY;
+                //     startTime = new Date().getTime();
+                //     e.preventDefault();
+                // });
+                // surface.addEventListener('mouseup', (e) => {
+                //     distX = e.pageX - startX;
+                //     distY = e.pageY - startY;
+                //     elapsedTime = new Date().getTime() - startTime;
+                //     if (elapsedTime <= allowedTime) {
+                //         if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                //             if (distX > 0) {
+                //                 if (isEnabled) {
+                //                     previousSlide(currentSlide);
+                //                 }
+                //             }
+                //             else {
+                //                 if (isEnabled) {
+                //                     nextSlide(currentSlide);
+                //                 }
+                //             }
+                //         }
+                //     }
+                // });
+                surface.addEventListener('touchstart', (e) => {
+                    if (e.target.classList.contains('arrow')) {
+                        if (e.target.classList.contains('arrow-prev')) {
+                            if (isEnabled) {
+                                previousSlide(currentSlide);
+                            }
+                        }
+                        else {
+                            if (isEnabled) {
+                                nextSlide(currentSlide);
+                            }
+                        }
+                    }
+                    else if (e.target.classList.contains('slide-phone')) {
+                        let screenOfLeftPhone = document.querySelector('#slide-phone-screen-1');
+                        let screenOfRightPhone = document.querySelector('#slide-phone-screen-2');
+                        if (e.target.classList.contains('slide-phone')) {
+                            if (e.target.classList.contains('slide-phone-1')) {
+                                turnScreenOnOrOff(screenOfLeftPhone);
+                            }
+                            else if (e.target.classList.contains('slide-phone-2')) {
+                                turnScreenOnOrOff(screenOfRightPhone);
+                            }
+                        }
+                        function turnScreenOnOrOff(screen) {
+                            if (screen.classList.contains('screen-off')) {
+                                screen.classList.remove('screen-off');
+                            }
+                            else {
+                                screen.classList.add('screen-off');
+                            }
+                        }
+                    }
+                    let touchObj = e.changedTouches[0];
+                    startX = touchObj.pageX;
+                    startY = touchObj.pageY;
+                    startTime = new Date().getTime();
+                    e.preventDefault();
+                });
+                surface.addEventListener('touchmove', (e) => {
+                    e.preventDefault();
+                });
+                surface.addEventListener('touchend', (e) => {
+                    let touchObj = e.changedTouches[0];
+                    distX = touchObj.pageX - startX;
+                    distY = touchObj.pageY - startY;
+                    elapsedTime = new Date().getTime() - startTime;
+                    if (elapsedTime <= allowedTime) {
+                        if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                            if (distX > 0) {
+                                if (isEnabled) {
+                                    previousSlide(currentSlide);
+                                }
+                            }
+                            else {
+                                if (isEnabled) {
+                                    nextSlide(currentSlide);
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+            swipeDetect(el);
         },
         modalBlock() {
             const FORM = document.querySelector('#contact-form');
@@ -154,6 +281,7 @@
 
         init() {
             this.activeLinks();
+            this.mobileMenu();
             this.modalBlock();
             this.changeActiveLinkDuringScroll();
             this.slider();
